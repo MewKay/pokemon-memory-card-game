89841945adb66c1b1cf5ed126c20b4a703b80e85
MewKay <miorakanto25z@gmail.com>
2024-10-17 16:15:46 +0300

    Create function to generate random IDs

--- src/components/Main/CardGrid.jsx
+++ src/components/Main/CardGrid.jsx
@@ -1,12 +1,28 @@
 import Card from "./Card";
 
+const generateArray = function generateArrayOf12RandomIDs() {
+  let array = [];
+  const ARRAY_MAX_LENGTH = 12;
+  const POKEMON_ID_START = 1;
+  const POKEMON_ID_END = 1024;
+
+  for (let i = 0; i < ARRAY_MAX_LENGTH; i += 1) {
+    const numberToInsert =
+      Math.floor(Math.random() * (POKEMON_ID_END - POKEMON_ID_START + 1)) +
+      POKEMON_ID_START;
+    array.push(numberToInsert);
+  }
+
+  return array;
+};
+
 const CardGrid = function CardGridComponent() {
-  let cards = new Array(12).fill(null);
+  let cards = generateArray();
 
   return (
     <div className="card-grid">
-      {cards.map((card, index) => (
-        <Card key={crypto.randomUUID()} />
+      {cards.map((pokemonID) => (
+        <Card key={pokemonID} />
       ))}
     </div>
   );
