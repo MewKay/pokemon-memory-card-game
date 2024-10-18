203f170a57041c23e5fba2aa0060f5f079eea407
MewKay <miorakanto25z@gmail.com>
2024-10-18 12:53:14 +0300

    Add style to scoreboard

--- src/components/Main/Scoreboard.jsx
+++ src/components/Main/Scoreboard.jsx
@@ -1,8 +1,10 @@
 const Scoreboard = function ScoreboardComponent({ bestScore, currentScore }) {
   return (
     <div className="scoreboard">
-      <p className="best-score">Best Score: {bestScore}</p>
-      <p className="current-score">Score: {currentScore}</p>
+      <div className="container">
+        <p className="best-score">Best Score: {bestScore}</p>
+        <p className="current-score">Score: {currentScore}</p>
+      </div>
     </div>
   );
 };
