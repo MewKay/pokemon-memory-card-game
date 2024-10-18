303e20efe234a1f2d68c1d8be5bf873db8ae61b7
MewKay <miorakanto25z@gmail.com>
2024-10-18 10:19:21 +0300

    Add style to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,11 @@
+import title from "../../assets/pokemonMemoryGameTitle.png";
+
 const Header = function HeaderComponent() {
   return (
     <header>
-      <h1>Pokemon Memory Card Game</h1>
+      <div className="title-container">
+        <img src={title} alt="Pokemon Memory Card Game" />
+      </div>
       <p>
         Click a card to gain a point. Do not click on the same card twice or you
         will loose the game. Try to get to 12 points to win!!
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import "./styles/reset.css";
+import "./styles/style.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
